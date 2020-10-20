9107e2173d2b9394cb9b97ce3c49d994a110bf5b
Fahrih Fatahilah <painchikusudo@gmail.com>
2020-10-20 12:46:46 +0800

    PBOIF2

---
+++ src/pboif2_10116405_latihan15_logika/PBOIF2_10116405_LATIHAN15_Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116405_latihan15_logika;
+
+/**
+ *
+ * @author Fahrih Fatahilah
+ * Nama  : Fahrih Fatahilah
+ * Kelas : IF-2 / PBO-IF2
+ * Nim   : 10116405
+ * 
+ */
+public class PBOIF2_10116405_LATIHAN15_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
